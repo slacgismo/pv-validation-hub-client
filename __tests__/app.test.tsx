a11efd482c7f4b78ce450107ce0ae9a2ef6600c7
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2024-05-06 16:40:05 -0700

    added import to test
    Signed-off-by: Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -1,11 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import {describe, it, expect} from '@jest/globals';
 import Home from '../src/app/page';
 
 describe('Home page', () => {
-    it('renders without crashing', () => {
-        render(<Home />);
-        const test = screen.getByText('Get started by editing');
-        console.log(test.textContent);
-        expect(test.textContent).toBe('Get started by editing');
-    });
+  it('renders without crashing', () => {
+    render(<Home />);
+    const test = screen.getByText('Get started by editing');
+    console.log(test.textContent);
+    expect(test.textContent).toBe('Get started by editing');
+  });
 });
