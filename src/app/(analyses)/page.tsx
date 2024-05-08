6abce5aaa13e26b45d76faf88aae8a7c4726ce34
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2024-05-08 10:53:52 -0700

    added basic import structure
    Signed-off-by: Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>

---
+++ src/app/(analyses)/page.tsx
@@ -0,0 +1,8 @@
+// *********** START OF IMPORTS ***********
+import Link from 'next/link';
+
+// *********** MODULE IMPORTS ***********
+
+// *********** REDUX IMPORTS ***********
+
+// *********** END OF IMPORTS ***********
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import Image from 'next/image';
+import Homepage from './homepage';
+
+// *********** END OF IMPORTS ***********
 
 /**
- * The Home component is the main page of the app.
- * It displays a welcome message and a link to Vercel's website.
- * @returns {JSX.Element} The rendered Home component.
+ * Render the homepage
+ * @return {JSX.Element} The homepage
  */
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing
-        </p>
-      </div>
-    </main>
+    <Homepage />
   );
 }
