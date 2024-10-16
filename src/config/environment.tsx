35dc5cfad4a7817897a15d9d0c3a7e10cb90d595
Mitchell Victoriano <mitchell.victoriano@gmail.com>
2024-10-16 04:23:06 -0700

    Frontend env should include 'localhost' for development and default to development not production

--- src/config/environment.tsx
+++ src/config/environment.tsx
@@ -10,13 +10,13 @@ const BASE_API_URL_APP_PRODUCTION = 'https://api.pv-validation-hub.org';
 
 const parseEnvironment = () => {
   if (typeof window !== 'undefined') {
-    if (window.location.hostname.includes('127.0.0.1')) {
+    if (window.location.hostname.includes('127.0.0.1') || window.location.hostname.includes('localhost')) {
       return ENVIRONMENT_DEVELOPMENT;
     }
     if (window.location.hostname.includes('staging')) {
       return ENVIRONMENT_STAGING;
     }
-    return ENVIRONMENT_PRODUCTION;
+    return ENVIRONMENT_DEVELOPMENT;
   } else {
     // During the build process, use process.env.NODE_ENV to determine the environment
     switch (process.env.NODE_ENV) {
@@ -25,7 +25,7 @@ const parseEnvironment = () => {
       case 'production':
         return ENVIRONMENT_PRODUCTION;
       default:
-        return ENVIRONMENT_PRODUCTION;
+        return ENVIRONMENT_DEVELOPMENT;
     }
   }
 };
